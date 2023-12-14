058f683ecff688415cea73bd5a1d94662c0c12ac
graial <allan.gray@sunEtrike.com>
2023-12-14 11:04:08 +0000

    readd unit-tests without submodule

---
+++ unit-tests/tests/MyFirstTest.cpp
@@ -0,0 +1,33 @@
+#include "CppUTest/TestHarness.h"
+
+//Compile error Failure occurs when uncommenting these 2 lines
+#include "ArduinoFake.h"
+using namespace fakeit;
+
+extern "C"
+{
+	/*
+	 * Add your c-only include files here
+	 */
+}
+
+TEST_GROUP(MyCode)
+{
+    void setup()
+    {
+      MemoryLeakWarningPlugin::saveAndDisableNewDeleteOverloads();
+    }
+
+    void teardown()
+    {
+    }
+};
+
+TEST(MyCode, test1)
+{
+    CHECK_EQUAL(1,1);
+     /*
+     * Instantiate your class, or call the function, you want to test.
+     * Then delete this comment
+     */
+}
